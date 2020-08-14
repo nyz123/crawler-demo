c946154bce98a729423f9255ed8a1fe0dbca322f
xiedongli <dongli.xie@verymro.com>
2020-08-14 08:35:47 +0800

    zkh

--- zkh/getZKH.py
+++ zkh/getZKH.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*- 
 import json
 import csv
 import requests
@@ -18,36 +19,31 @@ def get_data():
     cate_url = 'https://www.zkh360.com/servezkhApi/catalog/listIndexCatalogs?tranceId=602230911597140007244'
     pro_url = 'https://web.zkh360.com/api/search/listProductInfo'
 
-    csv_file = open('F:\\xdl\\github\\crawler-demo\\zkh\\data\\zkh_cate.csv','w')
+    csv_file = open('F:\\xdl\\github\\crawler-demo\\zkh\\data\\zkh_cate.csv','w',encoding='utf-8')
     writer = csv.writer(csv_file)
     writer.writerow(['品名','一级目录','二级目录','三级目录','四级目录', '品牌', '价格', '单位', '商品型号', '订货编码','规格参数'])
 
     cate_res =  requests.get(cate_url)
     cate_json = json.loads(cate_res.text)
     # 遍历1级分类
-    for i in range(0,1):
+    for i in range(0,len(cate_json)): # cate_json
         firstCatalog = cate_json[i]['firstCatalog']
         print('----------------------------------')
-        str = 'category:' + firstCatalog[0]['catalogName'] + ',' + firstCatalog[1]['catalogName']
-        if len(firstCatalog)==3:
-            str += ',' + firstCatalog[2]['catalogName']
-        print(str)
 
         childrenCatalog = cate_json[i]['childrenCatalog']
 
-        for j in range(0,1): # len(childrenCatalog)
+        for j in range(0,len(childrenCatalog)): # len(childrenCatalog)
             catalogName = childrenCatalog[j]['catalogName']
             clickImagePath = childrenCatalog[j]['clickImagePath']
-            path1 = 'F:\\xdl\\github\\crawler-demo\\zkh\\cates\\'+catalogName + '_clickImage.png'
-            download_img(clickImagePath,path1)
+            # path1 = 'F:\\xdl\\github\\crawler-demo\\zkh\\cates\\'+catalogName + '_clickImage.png'
+            # download_img(clickImagePath,path1)
             crmId = childrenCatalog[j]['crmId']
-            print(catalogName)
-            img_dir = 'F:\\xdl\\github\\crawler-demo\\zkh\\'+catalogName 
-            os.mkdir(img_dir)
+            # img_dir = 'F:\\xdl\\github\\crawler-demo\\zkh\\'+catalogName 
+            # os.mkdir(img_dir)
 
             page = 0
             total = 0
-            while page <= 10: # total
+            while page <= total: # total
                 page += 1
                 req = {
                     'from': page,
@@ -68,11 +64,11 @@ def get_data():
                     if pro_json and pro_json['page'] and pro_json['page']['content'] and pro_json['page']['totalPages']:
                         pro_content = pro_json['page']['content']
                         if page == 1:
-                            total = pro_json['page']['totalPages']                
+                            total = min(pro_json['page']['totalPages'],49)
                         for pro in pro_content:
                             # 品名 目录 品牌 价格 单位 商品型号 订货编码
                             proSkuProductName = pro['proSkuProductName'] 
-                            print(proSkuProductName)
+                            print(i,catalogName,j,total,page,proSkuProductName)
                             proBrandName = pro['proBrandName']
                             catalogs = pro['catalogs']['catalogs']
                             price = pro['price']
@@ -81,9 +77,9 @@ def get_data():
                             proSkuNo = pro['proSkuNo']
                             specificationList = pro['specificationList'] # 属性
                             proImgPath_Z1 = pro['proImgPath_Z1']
-                            for ii in range(len(proImgPath_Z1)):
-                                img_path = img_dir + '\\'+ proSkuNo + '_' + generate_random_str() + '.jpg'
-                                download_img(proImgPath_Z1[ii],img_path)
+                            # for ii in range(len(proImgPath_Z1)):
+                                # img_path = img_dir + '\\'+ proSkuNo + '_' + generate_random_str() + '.jpg'
+                                # download_img(proImgPath_Z1[ii],img_path)
                             specs = ''
                             for s in range(len(specificationList)):
                                 specs += specificationList[s]['proSpecName']+':'+specificationList[s]['specificationValue']
@@ -91,7 +87,6 @@ def get_data():
                                     specs += ' '
                             writer.writerow([proSkuProductName,catalogs[0]['catalogName'],catalogs[1]['catalogName'],catalogs[2]['catalogName'],catalogs[3]['catalogName'],proBrandName,price,unitOfMeasureCode,proMaterialNo,proSkuNo,specs])
 
-                    print(total,page)
 
     csv_file.close()
 
